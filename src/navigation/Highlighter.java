2c5323b51739fca1f0f0cbaaa76bc2c7c27f8406
Thibault Helsmoortel <thibault.helsmoortel@gmail.com>
2016-10-05 12:09:58 +0200

    Add handling for SessionNotFoundException

--- src/navigation/Highlighter.java
+++ src/navigation/Highlighter.java
@@ -5,12 +5,14 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.SessionNotFoundException;
 
 import java.io.File;
 import java.io.IOException;
 
 /**
  * Class responsible for (un)highlighting elements.
+ *
  * @author Thibault Helsmoortel
  */
 public final class Highlighter {
@@ -18,7 +20,7 @@ public final class Highlighter {
     private static final Logger LOGGER = Logger.getLogger(Highlighter.class);
 
     //Assuming JS is enabled
-    private static JavascriptExecutor js = (JavascriptExecutor)Navigator.getInstance().getDriver();
+    private static JavascriptExecutor js = (JavascriptExecutor) Navigator.getInstance().getDriver();
     private static WebElement lastElem = null;
     private static String lastBorder = null;
 
@@ -45,6 +47,7 @@ public final class Highlighter {
 
     /**
      * Highlights the specified element.
+     *
      * @param elem the element to highlight
      */
     public static void highlightElement(WebElement elem) {
@@ -53,7 +56,14 @@ public final class Highlighter {
         LOGGER.debug("Highlighting element: " + elem.toString());
         // remember the new element
         lastElem = elem;
-        lastBorder = (String)(js.executeScript(SCRIPT_GET_ELEMENT_BORDER, elem));
+        try {
+            lastBorder = (String) (js.executeScript(SCRIPT_GET_ELEMENT_BORDER, elem));
+        } catch (SessionNotFoundException e) {
+            //Should for some reason be handled. The exception was raised
+            // in the shouldReset test in the StatsTrackerTests class
+            LOGGER.debug("Session wasn't found, couldn't get the last border.");
+            lastBorder = null;
+        }
     }
 
     /**
@@ -67,6 +77,10 @@ public final class Highlighter {
                 js.executeScript(SCRIPT_UNHIGHLIGHT_ELEMENT, lastElem, lastBorder);
             } catch (StaleElementReferenceException ignored) {
                 //The page got reloaded, the element isn't there
+            } catch (SessionNotFoundException e) {
+                //Should for some reason be handled. The exception was raised
+                // in the shouldReset test in the StatsTrackerTests class
+                LOGGER.debug("Session wasn't found, couldn't unhighlight previous element.");
             } finally {
                 //Element either restored or wasn't valid, nullify in both cases
                 lastElem = null;
