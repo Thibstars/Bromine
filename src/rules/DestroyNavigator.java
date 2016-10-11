0b573f3343e1ec1fe924f90ab61af402c562b097
Thibault Helsmoortel <thibault.helsmoortel@gmail.com>
2016-10-11 10:04:54 +0200

    Add DestroyNavigator test rule

---
+++ src/rules/DestroyNavigator.java
@@ -0,0 +1,25 @@
+package rules;
+
+import navigation.NavigatorFactory;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+/**
+ * Rule responsible for destroying the navigator.
+ * @author Thibault Helsmoortel
+ */
+public class DestroyNavigator implements TestRule {
+
+    @Override
+    public Statement apply(final Statement statement, final Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                statement.evaluate();
+                //Destroy the navigator
+                NavigatorFactory.destroyNavigator();
+            }
+        };
+    }
+}
